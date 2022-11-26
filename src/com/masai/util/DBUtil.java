a28a44b93995c415af34ba45fac6e998ec091532
bhushan8554 <bhushantaru4297@gmail.com>
2022-11-27 01:06:56 +0530

    adding img

--- src/com/masai/util/DBUtil.java
+++ src/com/masai/util/DBUtil.java
@@ -22,7 +22,7 @@ public class DBUtil {
 		String url="jdbc:mysql://localhost:3306/bank";
 		
 		try {
-			conn= DriverManager.getConnection(url,"root","password");
+			conn= DriverManager.getConnection(url,"root","root");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
